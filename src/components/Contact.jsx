435f4ca40748936381cf2531985728104819f96b
Alan <60156381+alanbayolo@users.noreply.github.com>
2023-04-06 02:10:20 -0400

    added links to About

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,8 @@ const Contact = () => {
           <h2 className={styles.headingContact}>Get In Touch ✉️</h2>
           <div className="flex flex-col place-self-center gap-7 mt-7">
             <div className="flex flex-row place-self-center gap-4">
-              <a href=""><img src={linkedin} alt="LinkeIn" /></a>
-              <a href=""><img src={github} alt="GitHub" /></a>
+              <a href="https://linkedin.com/in/alanbayolo"><img src={linkedin} alt="LinkeIn" /></a>
+              <a href="https://github.com/alanbayolo/"><img src={github} alt="GitHub" /></a>
             </div>
             <a class="email-link" href="mailto:alanbayolo@hotmail.com" className="text-[#CAD1D8] text-[20px]">alanbayolo@hotmail.com</a>
             <SayHi />
